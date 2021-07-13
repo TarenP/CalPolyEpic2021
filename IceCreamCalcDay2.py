cfb8d584348ad8985f734721dd4d8c51c4559d80
Taren P <63258880+TarenP@users.noreply.github.com>
2021-07-13 11:59:39 -0700

    Day2

--- IceCreamCalcDay2.py
+++ IceCreamCalcDay2.py
@@ -1,8 +1,6 @@
 "Take a list of an order and use a function to calucate price. Store name and price into list for records"
 
 
-cupSize = ""
-addOnNum = 0
 RecordCost = []
 
 
@@ -10,7 +8,7 @@ cup = True
 addOn = False
 
 
-def order(Name, cupSize, addOnNum, recordCost):
+def order(Name, cupSize, addOnNum, StandardTops, FancyTops, recordCost):
     total = 0
     if addOnNum == "0":
         total += 0
@@ -20,12 +18,32 @@ def order(Name, cupSize, addOnNum, recordCost):
         total += 0.99
     elif cupSize == "3":
         total += 1.49
+    else:
+        print("incorrect number")
+        
 
     if cupSize == "2":
         total += 4.99
     elif cupSize == "1":
         total += 3.99
+    else:
+        print("incorrect number")
+        
+
+    if StandardTops <9 and StandardTops >1:
+        total += .29
+    elif StandardTops == 0:
+        total += 0
+    else:
+        print("incorrect number")
     
+    if FancyTops <5 and FancyTops >1:
+        total += .59
+    elif StandardTops == 0:
+        total += 0
+    else:
+        print("incorrect number")
+        
     RecordCost.append(Name)
     RecordCost.append(total)
 
@@ -37,8 +55,10 @@ while True:
             
     
     addOnNum = input("Add Ons.\n0. None\n1. Dipped Sugar Cone($0.59)\n2. Waffle Cone($0.99)\n3. Chocolate Dipped Waffle Cone($1.49)\n")
-        
+    
+    StandardTops = int(input("Standard Toppings.\n0. None\n1. Rainbow Sprinkles($0.29)\n2. Chocolate Chips($0.29)\n3. Whipped Cream($0.29)\n4. Waffle Cone Pieces($0.29)\n5. Chocolate Syrup($0.29)\n6. Strawberry Sauce($0.29)\n7. Brownie Chunks($0.29)\n8. Cookie Chunks($0.29)"))
+    FancyTops = int(input("Fancy Toppings.\n0. None\n1. Hot Fudge/Caramel($0.59)\n2. Nuts($0.59)\n3. Fruit($0.59)\n4. Candy($0.59)"))
     #Customers -= 1
-    order(Name, cupSize, addOnNum, RecordCost)
+    order(Name, cupSize, addOnNum, StandardTops, FancyTops, RecordCost)
     print(RecordCost)
 
