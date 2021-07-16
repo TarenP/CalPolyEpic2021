12344ce16349cd86d57bb91d684b159e0a210b5f
Taren P <63258880+TarenP@users.noreply.github.com>
2021-07-16 09:37:44 -0700

    Shake Table Data

--- ShakeTableData.py
+++ ShakeTableData.py
@@ -4,8 +4,15 @@ import matplotlib.pyplot as plt
 plotdata=pd.read_csv('Sensor Data CalPolyEpic 2021 - Data.csv')
 
 x=plotdata['Time']
-y=plotdata['ax']
-
-plt.plot(x, y)
+ax=plotdata['ax']
+ay=plotdata['ay']
+az=plotdata['az']
+plt.plot(x, ax, color='red', alpha = 0.6)
+plt.plot(x, ay, color='blue', alpha = 0.8)
+plt.plot(x,az, color='green', alpha = 0.7)
+plt.legend("xyz")
+plt.ylabel("Acceleration(g)")
+plt.xlabel("Time(s)")
+plt.title("Nonupgraded Building")
 plt.savefig('DataPlot.png')
 plt.show()
